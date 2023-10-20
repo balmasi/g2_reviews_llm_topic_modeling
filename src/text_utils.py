472deb2d54f2996889b38a9e6c282d58a9ba1635
Borna Almasi <bornaalmasi@bornas-mbp.home>
2023-10-20 01:09:31 -0400

    fix: import language model in a deployment-safe way

--- src/text_utils.py
+++ src/text_utils.py
@@ -1,8 +1,9 @@
 # Let's split the reviews into sentences so we can get a fine-grained view of the reviews
-import spacy
+
+import en_core_web_sm
 
 # Load the small English model. You can use the medium or large model for more accuracy but at the cost of speed.
-nlp = spacy.load("en_core_web_sm")
+nlp = en_core_web_sm.load()
 
 def split_into_sentences(text):
     # Use the model to split the text into sentences
